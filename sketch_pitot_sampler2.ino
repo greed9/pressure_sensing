099b84301da993ab60626c565ed0d5289acd16c3
greed9 <greed9@valenciacollege.edu>
2018-07-13 21:49:00 -0400

    Adding improved pitot sketch

---
+++ sketch_pitot_sampler2.ino
@@ -0,0 +1,356 @@
+#include <Wire.h>
+
+
+// defines for neopixel hsv color
+#define RED 0
+#define GREEN 1
+#define BLUE 2
+
+// Define i2c addr of bmp180
+#define BMP085_ADDRESS 0x77  // I2C address of BMP085
+
+// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
+// released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1
+#define PIN            3
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS      1
+
+// Must be pwm-capable pin.  Ask me how I know.
+#define METER_PIN       6
+
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
+// example for more information on possible values.
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+const unsigned char OSS = 0;  // Oversampling Setting
+
+// Calibration values
+int ac1;
+int ac2; 
+int ac3; 
+unsigned int ac4;
+unsigned int ac5;
+unsigned int ac6;
+int b1; 
+int b2;
+int mb;
+int mc;
+int md;
+
+// b5 is calculated in bmp085GetTemperature(...), this variable is also used in bmp085GetPressure(...)
+// so ...Temperature(...) must be called before ...Pressure(...).
+long b5; 
+
+short temperature;
+long pressure;
+
+byte state = 0 ;
+byte index = 0 ;
+long samples[250] = { 0 } ;
+long startTime = 0 ;
+long stopTime = 0 ;
+
+/******************************************************************************
+ * https://blog.adafruit.com/2012/03/14/constant-brightness-hsb-to-rgb-algorithm/
+ * accepts hue, saturation and brightness values and outputs three 8-bit color
+ * values in an array (color[])
+ *
+ * saturation (sat) and brightness (bright) are 8-bit values.
+ *
+ * hue (index) is a value between 0 and 767. hue values out of range are
+ * rendered as 0.
+ *
+ *****************************************************************************/
+void hsb2rgb(uint16_t index, uint8_t sat, uint8_t bright, uint8_t color[3])
+{
+  uint16_t r_temp, g_temp, b_temp;
+  uint8_t index_mod;
+  uint8_t inverse_sat = (sat ^ 255);
+
+  index = index % 768;
+  index_mod = index % 256;
+
+  if (index < 256)
+  {
+    r_temp = index_mod ^ 255;
+    g_temp = index_mod;
+    b_temp = 0;
+  }
+
+  else if (index < 512)
+  {
+    r_temp = 0;
+    g_temp = index_mod ^ 255;
+    b_temp = index_mod;
+  }
+
+  else if ( index < 768)
+  {
+    r_temp = index_mod;
+    g_temp = 0;
+    b_temp = index_mod ^ 255;
+  }
+
+  else
+  {
+    r_temp = 0;
+    g_temp = 0;
+    b_temp = 0;
+  }
+
+  r_temp = ((r_temp * sat) / 255) + inverse_sat;
+  g_temp = ((g_temp * sat) / 255) + inverse_sat;
+  b_temp = ((b_temp * sat) / 255) + inverse_sat;
+
+  r_temp = (r_temp * bright) / 255;
+  g_temp = (g_temp * bright) / 255;
+  b_temp = (b_temp * bright) / 255;
+
+  color[RED]  = (uint8_t)r_temp;
+  color[GREEN]  = (uint8_t)g_temp;
+  color[BLUE] = (uint8_t)b_temp;
+}
+
+
+
+void setup()
+{
+  pixels.begin(); // This initializes the NeoPixel library.
+  Serial.begin(9600);
+  //Serial.println ( "Starting" ) ;
+  
+  Wire.begin();
+  bmp085Calibration();
+  startTime = millis ( ) ;
+  //Serial.println( "done cal") ;
+  
+  pixels.setPixelColor(0, pixels.Color( 0,0,0)); 
+  pixels.show ( ) ;
+}
+
+#define DECAY 0.93
+#define MAX_PRESSURE_DIFF (500.0)
+#define MIN_PRESSURE_DIFF (-100.0)
+
+void loop()
+{
+    static long avgPressure = 0L ;
+    static float b = 1.0 - DECAY ;
+    static float ambientPressure = 0.0 ;
+    float diff = 0.0 ;
+    int pwmOutput = 0 ;
+    int hueOutput = 0 ;
+    uint8_t color[3] = { 0 } ;
+
+    // Read temp first, needed for bmp180 calibration
+    temperature = bmp085GetTemperature(bmp085ReadUT());
+
+    // Pressure is returned as hPa * 100 
+    pressure = bmp085GetPressure(bmp085ReadUP());
+
+    // Low-pass filter
+    avgPressure += b * ( pressure - avgPressure ) ;
+
+    // Figure difference between ambient and pitot sensors
+    // This will be proportional to air speed
+    diff = ( ( float ) avgPressure / 100L ) - ambientPressure ;
+
+    // Scale diff to  PWM value for meter
+    pwmOutput = map ( ( long ) diff * 100, ( int ) MIN_PRESSURE_DIFF, ( int ) MAX_PRESSURE_DIFF, 0, 255 ) ;
+
+    // Scale diff to hue for NeoPixel
+    hueOutput = map ( ( long ) diff * 100, ( int ) MIN_PRESSURE_DIFF, ( int ) MAX_PRESSURE_DIFF, 0, 768 ) ;
+    
+    // Read a new ambient pressure from the serial port if available.
+    // The recompute differential pressure and send back to serial port.
+    if( Serial.available ( ) )
+    {
+      long dummy = Serial.parseInt ( ) ;
+      ambientPressure = float ( dummy ) / 100.0 ;
+      diff = ( ( float ) avgPressure / 100L ) - ambientPressure ;
+      //Serial.print ( ambientPressure, DEC ) ;
+      //Serial.print ( "," ) ;
+      Serial.println ( diff, DEC ) ;
+      //Serial.print ( "," ) ;
+      //Serial.println ( ( float ) avgPressure / 100.0, DEC ) ;
+      
+      dummy = Serial.read ( ) ; // consume nl too
+      //Serial.println ( avgPressure, DEC ) ;
+    }
+
+    hsb2rgb(hueOutput, 255, 255, color ) ;
+    pixels.setPixelColor(0, pixels.Color( color[RED], color[GREEN], color[BLUE])); 
+    pixels.show ( ) ;
+    analogWrite ( METER_PIN, pwmOutput ) ;
+    delay( 100 ) ;
+   
+}
+
+// Stores all of the bmp085's calibration values into global variables
+// Calibration values are required to calculate temp and pressure
+// This function should be called at the beginning of the program
+void bmp085Calibration()
+{
+  ac1 = bmp085ReadInt(0xAA);
+  ac2 = bmp085ReadInt(0xAC);
+  ac3 = bmp085ReadInt(0xAE);
+  ac4 = bmp085ReadInt(0xB0);
+  ac5 = bmp085ReadInt(0xB2);
+  ac6 = bmp085ReadInt(0xB4);
+  b1 = bmp085ReadInt(0xB6);
+  b2 = bmp085ReadInt(0xB8);
+  mb = bmp085ReadInt(0xBA);
+  mc = bmp085ReadInt(0xBC);
+  md = bmp085ReadInt(0xBE);
+}
+
+// Calculate temperature given ut.
+// Value returned will be in units of 0.1 deg C
+short bmp085GetTemperature(unsigned int ut)
+{
+  long x1, x2;
+  
+  x1 = (((long)ut - (long)ac6)*(long)ac5) >> 15;
+  x2 = ((long)mc << 11)/(x1 + md);
+  b5 = x1 + x2;
+
+  return ((b5 + 8)>>4);  
+}
+
+// Calculate pressure given up
+// calibration values must be known
+// b5 is also required so bmp085GetTemperature(...) must be called first.
+// Value returned will be pressure in units of Pa.
+long bmp085GetPressure(unsigned long up)
+{
+  long x1, x2, x3, b3, b6, p;
+  unsigned long b4, b7;
+  
+  b6 = b5 - 4000;
+  // Calculate B3
+  x1 = (b2 * (b6 * b6)>>12)>>11;
+  x2 = (ac2 * b6)>>11;
+  x3 = x1 + x2;
+  b3 = (((((long)ac1)*4 + x3)<<OSS) + 2)>>2;
+  
+  // Calculate B4
+  x1 = (ac3 * b6)>>13;
+  x2 = (b1 * ((b6 * b6)>>12))>>16;
+  x3 = ((x1 + x2) + 2)>>2;
+  b4 = (ac4 * (unsigned long)(x3 + 32768))>>15;
+  
+  b7 = ((unsigned long)(up - b3) * (50000>>OSS));
+  if (b7 < 0x80000000)
+    p = (b7<<1)/b4;
+  else
+    p = (b7/b4)<<1;
+    
+  x1 = (p>>8) * (p>>8);
+  x1 = (x1 * 3038)>>16;
+  x2 = (-7357 * p)>>16;
+  p += (x1 + x2 + 3791)>>4;
+  
+  return p;
+}
+
+// Read 1 byte from the BMP085 at 'address'
+char bmp085Read(unsigned char address)
+{
+  unsigned char data;
+  
+  Wire.beginTransmission(BMP085_ADDRESS);
+  Wire.write(address);
+  Wire.endTransmission();
+  
+  Wire.requestFrom(BMP085_ADDRESS, 1);
+  while(!Wire.available())
+    ;
+    
+  return Wire.read();
+}
+
+// Read 2 bytes from the BMP085
+// First byte will be from 'address'
+// Second byte will be from 'address'+1
+int bmp085ReadInt(unsigned char address)
+{
+  unsigned char msb, lsb;
+  
+  Wire.beginTransmission(BMP085_ADDRESS);
+  Wire.write(address);
+  Wire.endTransmission();
+  
+  Wire.requestFrom(BMP085_ADDRESS, 2);
+  while(Wire.available()<2)
+    ;
+  msb = Wire.read();
+  lsb = Wire.read();
+  
+  return (int) msb<<8 | lsb;
+}
+
+// Read the uncompensated temperature value
+unsigned int bmp085ReadUT()
+{
+  unsigned int ut;
+  
+  // Write 0x2E into Register 0xF4
+  // This requests a temperature reading
+  Wire.beginTransmission(BMP085_ADDRESS);
+  Wire.write(0xF4);
+  Wire.write(0x2E);
+  Wire.endTransmission();
+  
+  // Wait at least 4.5ms
+  delay(5);
+  
+  // Read two bytes from registers 0xF6 and 0xF7
+  ut = bmp085ReadInt(0xF6);
+  return ut;
+}
+
+// Read the uncompensated pressure value
+unsigned long bmp085ReadUP()
+{
+  unsigned char msb, lsb, xlsb;
+  unsigned long up = 0;
+  
+  // Write 0x34+(OSS<<6) into register 0xF4
+  // Request a pressure reading w/ oversampling setting
+  Wire.beginTransmission(BMP085_ADDRESS);
+  Wire.write(0xF4);
+  Wire.write(0x34 + (OSS<<6));
+  Wire.endTransmission();
+  
+  // Wait for conversion, delay time dependent on OSS
+  delay(2 + (3<<OSS));
+  
+  // Read register 0xF6 (MSB), 0xF7 (LSB), and 0xF8 (XLSB)
+  Wire.beginTransmission(BMP085_ADDRESS);
+  Wire.write(0xF6);
+  Wire.endTransmission();
+  Wire.requestFrom(BMP085_ADDRESS, 3);
+  
+  // Wait for data to become available
+  while(Wire.available() < 3)
+    ;
+  msb = Wire.read();
+  lsb = Wire.read();
+  xlsb = Wire.read();
+  
+  up = (((unsigned long) msb << 16) | ((unsigned long) lsb << 8) | (unsigned long) xlsb) >> (8-OSS);
+  
+  return up;
+}
+
+
